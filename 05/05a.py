6a1c8dd0889a736053ce3460906ba1da012d062f
Rob Hailman <robhailman@gmail.com>
2022-12-05 23:13:18 -0500

    Complete day 5

---
+++ 05/05a.py
@@ -0,0 +1,42 @@
+from enum import Enum
+
+IN_FILE = "05/input.txt"
+
+
+class ParsingState(Enum):
+    INITIAL = 1
+    MOVES = 2
+
+
+if __name__ == "__main__":
+    state = ParsingState.INITIAL
+    stack_count = 0
+    stacks = []
+    with open(IN_FILE) as f:
+        for line in f:
+            if state == ParsingState.INITIAL:
+
+                # if on first, line determine number of stacks
+                if stack_count == 0:
+                    stack_count = int(len(line.strip("\n")) / 4) + 1
+                    stacks = [[] for x in range(stack_count)]
+
+                if len(line) == 1:
+                    state = ParsingState.MOVES
+                else:
+                    for idx in range(stack_count):
+                        if line[1 + idx * 4] >= "A":
+                            stacks[idx].insert(0, line[1 + idx * 4])
+            elif state == ParsingState.MOVES:
+                stack_to = int(line.split("to")[1]) - 1
+                stack_from = int(line.split("to")[0].split("from")[1]) - 1
+                move_count = int(line.split("to")[0].split("from")[0].split("move")[1])
+                while move_count > 0:
+                    item = stacks[stack_from].pop()
+                    stacks[stack_to].append(item)
+                    move_count -= 1
+
+    answer = ""
+    for stack in stacks:
+        answer += stack[-1]
+    print(answer)
