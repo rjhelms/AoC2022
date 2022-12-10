e4e2d6d67f4e9d9bf43c06f7ee646e05411e66d5
Rob Hailman <robhailman@gmail.com>
2022-12-10 16:48:30 -0500

    Complete day 10

---
+++ 10/10.py
@@ -0,0 +1,52 @@
+from enum import Enum
+
+
+class ProcState(Enum):
+    FETCH = 1
+    ADD = 2
+
+
+IN_FILE = "10/input.txt"
+
+if __name__ == "__main__":
+    state = ProcState.FETCH
+    x_reg = 1
+    cycle = 0
+    running = True
+    line = None
+    param = None
+    result = 0
+    output = ""
+    with open(IN_FILE) as f:
+        while running == True:
+            # get cycle
+            if state == ProcState.FETCH:
+                line = f.readline()
+                if line == "":
+                    running = False
+                    break
+
+            # during cycle
+            cycle += 1
+            disp_cycle = (cycle % 40) - 1
+            if abs(disp_cycle - x_reg) <= 1:
+                output += "#"
+            else:
+                output += " "
+            if disp_cycle < 0:
+                print(output)
+                output = ""
+
+            if cycle == 20 or (cycle - 20) % 40 == 0:
+                result += cycle * x_reg
+
+            # do cycle
+            if state == ProcState.FETCH:
+                if line.split()[0] == "addx":
+                    param = int(line.split()[1])
+                    state = ProcState.ADD
+            else:
+                x_reg += param
+                state = ProcState.FETCH
+
+    print(result)
