afa2295d89f6e936630fb7fa7e3eeb588460fa96
Rob Hailman <robhailman@gmail.com>
2022-12-15 23:48:29 -0500

    Complete day 12

---
+++ 12/12a.py
@@ -0,0 +1,67 @@
+import math
+
+IN_FILE = "12/input.txt"
+
+
+class Node:
+    def __init__(self, position: tuple[int, int], height: int) -> None:
+        self.position = position
+        self.height = height
+        self.neighbours = []
+        self.distance = math.inf
+
+    def find_neighbours(self, node_dict: dict) -> None:
+        x_coord = self.position[0]
+        y_coord = self.position[1]
+        for possible_neighbour in [
+            (x_coord - 1, y_coord),
+            (x_coord + 1, y_coord),
+            (x_coord, y_coord - 1),
+            (x_coord, y_coord + 1),
+        ]:
+            if (
+                possible_neighbour in node_dict
+                and node_dict[possible_neighbour].height <= self.height + 1
+            ):
+                self.neighbours.append(node_dict[possible_neighbour])
+
+
+if __name__ == "__main__":
+    start_node = None
+    end_node = None
+    node_dict = {}
+    with open(IN_FILE) as f:
+        y_coord = 0
+        for line in [x.strip() for x in f]:
+            x_coord = 0
+            for character in line:
+                node = Node((x_coord, y_coord), ord(character) - 97)
+                if character == "S":
+                    node.height = 0
+                    start_node = node
+                elif character == "E":
+                    node.height = 25
+                    end_node = node
+                node_dict[(x_coord, y_coord)] = node
+                x_coord += 1
+            y_coord += 1
+
+    for coords in node_dict:
+        node_dict[coords].find_neighbours(node_dict)
+
+    unvisited_set = list(node_dict.values())
+    start_node.distance = 0
+    finished = False
+    while not finished:
+        unvisited_set.sort(key=lambda x: x.distance)
+        current_node = unvisited_set[0]
+        if current_node == end_node:
+            finished = True
+            break
+        neighbour_distance = current_node.distance + 1
+        for neighbour in current_node.neighbours:
+            if neighbour.distance > neighbour_distance:
+                neighbour.distance = neighbour_distance
+        unvisited_set.remove(current_node)
+
+    print(unvisited_set[0].distance)
